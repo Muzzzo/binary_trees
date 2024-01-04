1f501b11528afca1f58d6886601fff2adc7be6f6
musondachristopher@rocketmail.com <musondac1995@gmail.com>
2024-01-04 15:54:30 +0100

    3-del

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_delete - A funcxn that deletes a binary tree
- * @tree: A pointer to the root node of the tree to delete.
- * 
- * Return: If tree is NULL, do nothing.
+ * binary_tree_delete - A function that deletes a binary tree
+ * @tree: A pointer to the root node of the tree to delete
+ *
+ * Return: If the tree is NULL - do nothing
  */
 
 void binary_tree_delete(binary_tree_t *tree)
