cab768d8bcb55a3b67af9012cacd7bd9941fda4c
musondachristopher@rocketmail.com <musondac1995@gmail.com>
2024-01-04 13:38:10 +0100

    5-main.c

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_right - A function that inserts a node as the right-child
+ *                            of another node.
+ * @parent: is a pointer to the node to insert the right-child in.
+ * @value: is the value to store in the new node.
+ *
+ * Return: a pointer to the created node, or NULL on failure or if parent is NULL.
+ *
+ * Description: if a parent already has a right-child, the new node
+ *              takes its place and the old right-child is set as
+ *              the right-child of the new node.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		new->right = parent->right;
+		parent->right->parent = new;
+	}
+	parent->right = new;
+
+	return (new);
+}
---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf -A function that checks if a node is a leaf of a binary tree or no.
+ * @node: A pointer to the node to check.
+ *
+ * Return: If the node is a leaf return 1, Otherwise return 0.
+ * Descripxn: If node is NULL, return 0.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL || node->left != NULL || node->right != NULL)
+		return (0);
+
+	return (1);
+}
